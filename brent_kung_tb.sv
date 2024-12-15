00fcddea682c2e72079c57dedd8bed40a443fbd8
Abdelrahman Essam <133209653+AbdelrahmanEA8@users.noreply.github.com>
2024-12-15 22:56:49 +0200

    Update brent_kung_tb.sv

--- brent_kung_tb.sv
+++ brent_kung_tb.sv
@@ -1,31 +1,41 @@
-module Brent_kung_tb #(parameter WIDTH = 64)();
-    reg [WIDTH-1:0] a,b;
+module Brent_kung_tb #(parameter N = 64)();
+    reg [N-1:0] a,b;
     reg cin;
-    wire [WIDTH-1:0] sum;
+    wire [N-1:0] sum;
     wire cout;
 
     parameter MAXPOS = 255;
     parameter ZERO = 0;
 
-    Brent_kung_64bit DUV (.a(a),.b(b),.cin(cin),.sum(sum),.cout(cout));
+    Brent_kung_Nbit #(.N(N)) DUV (.a(a),.b(b),.cin(cin),.sum(sum),.cout(cout));
 
     initial begin
-        a=ZERO; b=ZERO; cin=ZERO;
+        // Directed test cases
+        a=ZERO; b=ZERO; cin=ZERO;       // 0+0=0
         #10;
-        a=MAXPOS; b=ZERO; cin=ZERO;
+        a=MAXPOS; b=ZERO; cin=ZERO;     // 255+0=255
         #10;
-        a=ZERO; b=MAXPOS; cin=ZERO;
+        a=ZERO; b=MAXPOS; cin=ZERO;     // 0+255=255
         #10;
-        a=MAXPOS; b=MAXPOS; cin=ZERO;
+        a=MAXPOS; b=MAXPOS; cin=ZERO;   // 255+255=510
         #10;
-        a=ZERO; b=ZERO; cin=1;
+        a=ZERO; b=ZERO; cin=1;          // 0+0+1=1
         #10;
-        a=MAXPOS; b=ZERO; cin=1;
+        a=MAXPOS; b=ZERO; cin=1;        // 255+0+1=256
         #10;
-        a=ZERO; b=MAXPOS; cin=1;
+        a=ZERO; b=MAXPOS; cin=1;        // 255+0+1=256
         #10;
-        a=MAXPOS; b=MAXPOS; cin=1;
+        a=MAXPOS; b=MAXPOS; cin=1;      // 255+255+1=511
         #10;
+
+        // Chech functionality using small numbers
+        repeat(100)begin
+            a=$urandom_range(0,1000000000);
+            b=$urandom_range(0,1000000000);
+            cin=$random;
+            #10;
+        end
+        // Chech Cout using random test cases
         repeat(100)begin
             a=$random;
             b=$random;
@@ -33,5 +43,8 @@ module Brent_kung_tb #(parameter WIDTH = 64)();
             #10;
         end
         $stop;
+    end
+    initial begin
+        $monitor("At time %t, a = %d, b = %d, cin = %b, sum = %d, cout = %b",$time, a, b, cin, sum, cout);
     end
 endmodule
